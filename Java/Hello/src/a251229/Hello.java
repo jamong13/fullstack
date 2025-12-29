6f232723438c6ac0c943c0cc38b04c1b582f7c55
jamong13 <limehs0618@gmail.com>
2025-12-29 13:17:38 +0900

    a251229-1

---
+++ Java/Hello/src/a251229/Hello.java
@@ -0,0 +1,16 @@
+package a251229;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+        // java 실행하면 제일 먼저 main 메서드를 실행한다.
+        // public: 접근제어자 : 외부에서 접근할 수 있는 객체
+        // private -> protected -> public
+        /* 
+            static: 정적 메서드 -> 객체를 만들지 않고도 사용 가능,
+                    공통으로 하나만 존재,
+                    메모리에 한 번만 생성
+        */
+        // void: 반환 값이 없다.
+    }
+}
---
+++ Java/Hello/src/a251229/ex01.java
@@ -0,0 +1,20 @@
+package a251229;
+
+public class ex01 {
+    public static void main(String[] args) {
+        // 1. **변수 선언 및 출력**
+        // - 이름, 나이, 키, 몸무게를 변수로 선언하고 출력하는 프로그램을 작성하세요.
+
+        String name = "이희수";
+        int age = 29;
+        double height = 157.8;
+        double weight = 0.0;
+
+        System.out.println("===학생 정보===");
+        System.out.println("이름: " + name);
+        System.out.println("나이: " + age + "세");
+        System.out.println("키: " + height + "cm");
+        System.out.println("몸무게: " + weight + "kg");
+
+    }
+}
